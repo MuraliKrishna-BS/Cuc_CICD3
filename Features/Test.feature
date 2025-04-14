640f7902729ff40eb80c6b64deea8596b17ec284
Muralikrishna <Muralikrishna@MURALI-LAPTOP>
2025-04-14 19:40:15 +0530

    Adding to GIT

---
+++ Features/Test.feature
@@ -0,0 +1,5 @@
+Feature: Test Feature1
+
+Scenario: Test Scenario for Feature1
+
+Given Test Step for Scenario of Feature1
---
+++ src/test/java/StepDefinition/Testccu1.java
@@ -0,0 +1,26 @@
+package StepDefinition;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+
+public class Testccu1 {
+
+	WebDriver Driver = new ChromeDriver();
+	
+	
+	@Given("Test Step for Scenario of Feature1")
+	public void Test_Step_for_Scenario_of_Feature1() throws InterruptedException {
+		
+		Driver.get("https://www.google.com");
+		
+		Thread.sleep(5000);
+		
+		Driver.quit();
+	  
+	  // url is given in Hook
+	}
+
+
+
+}
