6e730f2e0f805216b98252417b2c3db967b0920f
Muralikrishna <Muralikrishna@MURALI-LAPTOP>
2025-04-15 15:34:59 +0530

    push

--- src/test/java/StepDefinition/Testccu1.java
+++ src/test/java/StepDefinition/Testccu1.java
@@ -1,18 +1,23 @@
 package StepDefinition;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.cucumber.java.en.Given;
 
 public class Testccu1 {
 
-	WebDriver Driver = new EdgeDriver();
+	
+	ChromeOptions Opt = new ChromeOptions();
+	//optiion.addArguments("--headless=new");
+
 	
 	
 	@Given("Test Step for Scenario of Feature1")
 	public void Test_Step_for_Scenario_of_Feature1() throws InterruptedException {
 		
+		//Opt.addArguments("--headless=new");
+		
+		WebDriver Driver = new ChromeDriver(Opt);
 		Driver.get("https://www.google.com");
 		
 		Thread.sleep(5000);
